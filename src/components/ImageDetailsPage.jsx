6e133374ec1a5f919ab1abe884778f0e05006d02
Emily Sellers <emilymelindasellers@gmail.com>
2023-06-08 10:44:01 -0700

    Add `ImageDetailsPage` component with placeholder content

---
+++ src/components/ImageDetailsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ImageDetailsPage = () => {
+	return (
+		<>
+			<div>title</div>
+			<div>artist</div>
+			<div>image</div>
+			<div>back button</div>
+		</>
+	);
+};
+
+export default ImageDetailsPage;
