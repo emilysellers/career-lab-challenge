bf84a7b13c44093d5a0bc3a4c4ffb875bbbe86a5
Emily Sellers <emilymelindasellers@gmail.com>
2023-06-08 11:50:23 -0700

    Conditionally render ImageDetailsPage if artwork is selected or search results if no artwork is selected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,11 @@ import { SearchForm } from './SearchForm';
 import { Footer } from './Footer';
 
 import './App.css';
-// import ImageDetailsPage from './ImageDetailsPage.jsx';
+import ImageDetailsPage from './ImageDetailsPage.jsx';
 
 export function App() {
 	const [searchResults, setSearchResults] = useState([]);
-	const [selectedArtworkId, setSelectedArtworkId] = useState('');
+	const [selectedArtwork, setSelectedArtwork] = useState('');
 
 	function onSearchSubmit(query) {
 		// Search for the users's query.
@@ -20,28 +20,34 @@ export function App() {
 		// @see: ./src/api.js
 		searchArtworks(query).then((json) => {
 			const artworksData = json.data;
-			// console.log(artworksData);
 			setSearchResults(artworksData); // save search results to state
 		});
 	}
 
+	// Conditionally render search results if no artwork is selected or ImageDetailsPage if artwork is selected
 	return (
 		<div className="App">
 			<h1>TCL Career Lab Art Finder</h1>
 			<SearchForm onSearchSubmit={onSearchSubmit} />
-			<ul>
-				{searchResults.map((artwork) => (
-					<a
-						href={`#${artwork.image_id}`}
-						onClick={() => setSelectedArtworkId(artwork.image_id)}
-					>
-						<li key={artwork.title}>
-							{artwork.title} by{' '}
-							{artwork.artist_title == null ? 'unknown' : artwork.artist_title}
-						</li>
-					</a>
-				))}
-			</ul>
+			{selectedArtwork === '' ? (
+				<ul>
+					{searchResults.map((artwork, index) => (
+						<a
+							href={`#${artwork.image_id}`}
+							onClick={() => setSelectedArtwork(searchResults[index])}
+						>
+							<li key={artwork.image_id}>
+								{artwork.title} by{' '}
+								{artwork.artist_title == null
+									? 'unknown'
+									: artwork.artist_title}
+							</li>
+						</a>
+					))}
+				</ul>
+			) : (
+				<ImageDetailsPage selectedArtwork={selectedArtwork} />
+			)}
 			<Footer />
 		</div>
 	);
